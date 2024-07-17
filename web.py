60f9ee2dbe387e68a4677e28438928322fd5c46d
amit2507a <amitkhade2507@gmail.com>
2024-07-17 18:05:00 +0530

    aaaaa

--- web.py
+++ web.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 current_dir = Path(__file__).parent if "__file__" in locals() else Path.cwd()
 css_file = current_dir / "style" / "main.css"
-resume_file = current_dir / "assets\Amit_resume_businessAnalyst.pdf"
+resume_file = current_dir / "assets" / "AmitResumeBusinessAnalyst.pdf" 
 profile_pic = current_dir / "assets" / "ID size photo.jpg"
 
 # --- GENERAL SETTINGS ---
